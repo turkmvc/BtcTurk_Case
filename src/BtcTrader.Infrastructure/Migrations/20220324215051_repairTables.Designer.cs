8f4edf5ce830c3e2d309b01bd00b8df7e9cf2133
Fatih Gürdal <fgurdal@bimser.com.tr>
2022-03-25 00:51:37 +0300

    NotificationText kolon veri tipi migration ile düzeltildi

--- src/BtcTrader.Infrastructure/Migrations/20220324215051_repairTables.Designer.cs
+++ src/BtcTrader.Infrastructure/Migrations/20220324215051_repairTables.Designer.cs
@@ -4,6 +4,7 @@ using BtcTrader.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BtcTrader.Infrastructure.Migrations
 {
     [DbContext(typeof(BtcTraderContext))]
-    partial class BtcTraderContextModelSnapshot : ModelSnapshot
+    [Migration("20220324215051_repairTables")]
+    partial class repairTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/BtcTrader.Infrastructure/Migrations/20220324215051_repairTables.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BtcTrader.Infrastructure.Migrations
+{
+    public partial class repairTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "NotificationText",
+                table: "NotificationHistories",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "NotificationText",
+                table: "NotificationHistories",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+        }
+    }
+}
